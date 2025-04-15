5110921defe01e32f3a8bd4e7e6fccb9bddc2622
donnabell-s <donnasembrano03@gmail.com>
2025-04-15 08:49:14 +0800

    router

---
+++ frontend/src/app/features/buyer/search-properties/search-container/search-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-container',
+  imports: [],
+  templateUrl: './search-container.component.html',
+  styleUrl: './search-container.component.css'
+})
+export class SearchContainerComponent {
+
+}
